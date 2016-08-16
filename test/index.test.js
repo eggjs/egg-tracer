872cc110c43474416baaafe88490e8b8d12dbaf7
fengmk2 <m@fengmk2.com>
2016-08-16 13:09:03 +0800

    test: add lib test case

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require('assert');
+const Tracer = require('..');
+
+describe('index.test.js', () => {
+  it('should work with lib', () => {
+    assert(typeof Tracer === 'function');
+  });
+});
